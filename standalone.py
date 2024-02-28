31d1355325cc9940e1098f2c4fdf2d4e380262dc
chklff <chklff@gmail.com>
2024-02-28 22:55:24 +0000

    update

--- standalone.py
+++ standalone.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, jsonify
 import cx_Oracle
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 # Set Oracle client directory relative to the script file
-oracle_client_dir = os.path.join(os.path.dirname(__file__), 'instantclient_21_11')
+oracle_client_dir = os.getenv('ORACLE_CLIENT_DIR', os.path.join(os.path.dirname(__file__), 'instantclient_21_11'))
 os.environ["LD_LIBRARY_PATH"] = oracle_client_dir
 os.environ["ORACLE_HOME"] = oracle_client_dir
 
-# You might need to reconfigure environment variables for Python to recognize them.
-# This can depend on your operating system and setup.
-
-ORACLE_DSN = cx_Oracle.makedsn('165.22.180.189', '1521', 'ORCLCDB')
-CONNECTION = cx_Oracle.connect('system', 'iDx16reA2R9#i', ORACLE_DSN)
+# Use environment variables for sensitive information
+ORACLE_DSN = cx_Oracle.makedsn(*os.getenv('ORACLE_DSN').split('/'))
+CONNECTION = cx_Oracle.connect(os.getenv('ORACLE_USER'), os.getenv('ORACLE_PASSWORD'), ORACLE_DSN)
 
 @app.route('/query', methods=['POST'])
 def query_oracle():
